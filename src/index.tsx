64907785dcdaae5b77335b49b79e6f13a3a86dfb
webcrazy003 <raycoaster99001@gmail.com>
2020-01-24 09:56:36 -0800

    Configure Store and Route

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Provider } from 'react-redux';
+import { Route, BrowserRouter } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import configureStore from './reducers/store';
+import { PrimaryLayout } from './containers/layout';
+
+import './index.css';
+
+const root = document.getElementById('root');
+const store = configureStore();
+
+ReactDOM.render(
+  <Provider store={store}>
+    <div id="app">
+      <BrowserRouter>
+        <Route path="/" component={PrimaryLayout} />
+      </BrowserRouter>
+    </div>
+  </Provider>,
+  root,
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
