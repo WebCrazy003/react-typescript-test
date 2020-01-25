bde813ea0fdfcf9eafe563f4b52dbd610b8f503d
webcrazy003 <raycoaster99001@gmail.com>
2020-01-24 17:35:28 -0800

    Convert CSS to Styled Component on Navbar

--- src/containers/layout/navbar/index.tsx
+++ src/containers/layout/navbar/index.tsx
@@ -1,17 +1,28 @@
 import React from 'react';
+import styled from 'styled-components';
 import { Layout } from 'antd';
 
 import { Logo } from '../../../assets';
-import './index.css';
 
 const { Header } = Layout;
 
+const StyledDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+const StyledH2 = styled.h2`
+  color: #fff;
+  margin: 0px;
+`;
+
 const Navbar = (): JSX.Element => (
   <Header>
-    <div className="wrapper">
+    <StyledDiv>
       <img width={40} height={40} alt="Logo" src={Logo} />
-      <h2 style={{ color: 'white', margin: '0px' }}>Ant Design</h2>
-    </div>
+      <StyledH2>Ant Design</StyledH2>
+    </StyledDiv>
   </Header>
 );
 
