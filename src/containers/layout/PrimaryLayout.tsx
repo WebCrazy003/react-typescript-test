dccf2fe5b07f78c5d2dc135245f79dfbe27f17c1
webcrazy003 <raycoaster99001@gmail.com>
2020-01-24 16:48:55 -0800

    feat: Add Navbar and Footer on PrimaryLayout

--- src/containers/layout/PrimaryLayout.tsx
+++ src/containers/layout/PrimaryLayout.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { Layout } from 'antd';
 
 import { ContainerSignin } from '../auth';
+import { Navbar } from '.';
+
+const { Footer, Content } = Layout;
 
 class PrimaryLayout extends React.Component {
   render(): JSX.Element {
     return (
-      <Switch>
-        <Route path="/" component={ContainerSignin} />
-      </Switch>
+      <Layout className="layout">
+        <Navbar />
+        <Content style={{ padding: '0 50px' }}>
+          <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>
+            <Switch>
+              <Route path="/" component={ContainerSignin} />
+            </Switch>
+            <div style={{ height: '100px' }} />
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>Ant Design @2020 Created by Kevin</Footer>
+      </Layout>
     );
   }
 }
