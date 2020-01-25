83a1bf3bd1b80a4ac2f5f6f06fa8a3fe36a7672b
webcrazy003 <raycoaster99001@gmail.com>
2020-01-24 17:35:01 -0800

    Convert style attr to styled-comp on PrimaryLayout

--- src/containers/layout/PrimaryLayout.tsx
+++ src/containers/layout/PrimaryLayout.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 import { Switch, Route } from 'react-router-dom';
 import { Layout } from 'antd';
 
@@ -7,20 +8,38 @@ import { Navbar } from '.';
 
 const { Footer, Content } = Layout;
 
+const StyledContent = styled(Content)`
+  padding: 0 50px;
+`;
+
+const StyledDiv1 = styled.div`
+  background: #fff;
+  padding: 24px;
+  min-height: 280px;
+`;
+
+const StyledDiv2 = styled.div`
+  height: 100px;
+`;
+
+const StyledFooter = styled(Footer)`
+  text-align: center;
+`;
+
 class PrimaryLayout extends React.Component {
   render(): JSX.Element {
     return (
       <Layout className="layout">
         <Navbar />
-        <Content style={{ padding: '0 50px' }}>
-          <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>
+        <StyledContent>
+          <StyledDiv1>
             <Switch>
               <Route path="/" component={ContainerSignin} />
             </Switch>
-            <div style={{ height: '100px' }} />
-          </div>
-        </Content>
-        <Footer style={{ textAlign: 'center' }}>Ant Design @2020 Created by Kevin</Footer>
+            <StyledDiv2 />
+          </StyledDiv1>
+        </StyledContent>
+        <StyledFooter>Ant Design @2020 Created by Kevin</StyledFooter>
       </Layout>
     );
   }
